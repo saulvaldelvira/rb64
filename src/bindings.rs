025f69ad0c20eb17bf3119babc73e4297beb12d3
Saúl Valdelvira <saul@saulv.es>
2025-01-04 02:40:21 +0100

    Fix bindings

--- src/bindings.rs
+++ src/bindings.rs
@@ -1,9 +1,8 @@
-use core::ffi::c_size_t;
+use crate::prelude::Box;
 use core::slice;
-use std::ffi::c_ulonglong;
-use std::os::raw::c_ulonglong;
-use std::ptr;
-use std::{ffi::{c_char, CStr}, mem};
+use core::ffi::c_ulonglong;
+use core::ptr;
+use core::{ffi::{c_char, CStr}, mem};
 
 use crate::{decode, encode};
 
@@ -14,32 +13,41 @@ fn ptr_2_box<T>(ptr: *mut T, len: usize) -> Box<[T]> {
     }
 }
 
-fn box_2_ptr<T>(mut data: Box<[T]>) -> *mut T {
-    let elems = data.as_mut_ptr();
-    mem::forget(data);
-    elems
+#[repr(C)]
+pub struct Buffer {
+    ptr: *mut u8,
+    len: usize
 }
 
 #[no_mangle]
 pub extern "C"
-fn base64_decode(ptr: *const c_char, len: *mut c_ulonglong) -> *mut u8 {
+fn base64_decode(ptr: *const c_char, len: *mut c_ulonglong) -> Buffer {
     let cstr = unsafe { CStr::from_ptr(ptr) };
     if let Ok(s) = cstr.to_str() {
-        if let Ok(d) = decode(s) {
+        if let Ok(mut d) = decode(s) {
             unsafe { *len = d.len() as c_ulonglong };
-            return box_2_ptr(d)
+            let len = d.len();
+            let ptr = d.as_mut_ptr();
+            mem::forget(d);
+            return Buffer {
+                ptr,
+                len
+            }
         }
     }
-    ptr::null_mut()
+    Buffer {
+        ptr: ptr::null_mut(),
+        len: 0
+    }
 }
 
 #[no_mangle]
 pub extern "C"
 fn base64_buffer_free(res: Buffer) {
-    let Buffer { elems, elems_size } = res;
-    if elems.is_null() { return }
+    let Buffer { ptr, len } = res;
+    if ptr.is_null() { return }
 
-    let vec = ptr_2_box(elems, elems_size);
+    let vec = ptr_2_box(ptr, len);
     mem::drop( vec );
 }
 
@@ -50,13 +58,14 @@ fn base64_encode(ptr: *mut u8, len: usize) -> Buffer {
     let mut result = encode(&vec);
     result.push('\0');
 
+    let len = result.len();
     let mut result = result.into_boxed_str();
-    let buf = result.as_mut_ptr();
+    let ptr = result.as_mut_ptr();
     mem::forget(result);
 
-    EncodeResult {
-        buf,
+    Buffer {
+        ptr,
         len
-   }
+    }
 }
 
